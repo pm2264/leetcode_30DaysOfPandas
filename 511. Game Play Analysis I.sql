7be821f49fcff2ad6cf569d885b5bb22ee38e210
Pavitra Mohanarajan <114989541+pm2264@users.noreply.github.com>
2023-08-30 07:22:30 +0530

    Create 511. Game Play Analysis I.sql

---
+++ 511. Game Play Analysis I.sql
@@ -0,0 +1,6 @@
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    return activity.groupby(
+        'player_id'
+    ).agg(
+        first_login=('event_date', 'min')
+    ).reset_index()
