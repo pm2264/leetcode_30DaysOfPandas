1d24072461d5675068279064e33b11b6ac27b18a
Pavitra Mohanarajan <114989541+pm2264@users.noreply.github.com>
2023-08-30 07:28:09 +0530

    Create 1280. Students and Examinations.sql

---
+++ 1280. Students and Examinations.sql
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
+    
+    df1 = students.merge(subjects, how='cross')
+    
+    df2 = examinations.groupby(['student_id', 'subject_name']).agg(
+        attended_exams=('subject_name', 'count')
+    ).reset_index()
+    
+    result = df1.merge(df2, on=['student_id','subject_name'], how='left').sort_values(by = ['student_id', 'subject_name'])
+    
+    return result.fillna(0)[['student_id', 'student_name', 'subject_name', 'attended_exams']]
